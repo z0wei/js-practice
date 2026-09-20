cd0439ccd5f81891909de1a022893eefa64b443d
z0wei <zowei59@gmail.com>
2026-09-20 17:17:44 +0500

    '06-Conditional_branching:if,?'

---
+++ 06-Conditional_branching:if,?/task2.js
@@ -0,0 +1,6 @@
+let vopros = prompt("Какое официальное название JavaScript?", "");
+if (vopros == "ECMAScript") {
+    alert("Верно!");
+} else {
+    alert("Не знаете? ECMAScript!");
+} 
---
+++ 06-Conditional_branching:if,?/task3.js
@@ -0,0 +1,8 @@
+let www = prompt("Ведите число", "");
+if (www > 0) {
+    alert(1);
+} else if (www < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
