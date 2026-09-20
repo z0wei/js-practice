913a749b8bb1b797519a190cce7356daa869a3ba
z0wei <zowei59@gmail.com>
2026-09-20 13:43:35 +0500

    04-Basic_operators_mathematics

---
+++ 04-Basic_operators_mathematics/task1.js
@@ -0,0 +1,6 @@
+let a = 1, b = 2;
+let c = ++a; // a = 2, c = 2
+let d = b++; // b = 3, d = 2
+console.log(a, b, c, d)
+
+// 2 3 2 2
---
+++ 04-Basic_operators_mathematics/task4.js
@@ -0,0 +1,3 @@
+let a = prompt("Enter a number:", 1);
+let b = prompt("Enter another number:", 2);
+console.log(+a + +b); // 3 
